8716cbc1422c1d0a4667fe9cde06a9114c3df4e0
alexandrerussi <alexandrerussijunior@gmail.com>
2026-09-15 08:58:46 -0300

    aula 9 v1

---
+++ aula09-mini-crm/app.py
@@ -0,0 +1,68 @@
+from model import model_lead
+import control
+
+def add_lead():
+    name = input("Nome: ")
+    email = input("E-mail: ")
+    company = input("Empresa: ")
+    stage = input("Estágio de vendas: ")
+
+    if not name or not email or "@" not in email:
+        print("Nome e/ou e-mail válido são obrigatórios")
+        return
+
+    # precisar chamar model para modelar os dados
+    print(model_lead(name, company, email, stage))
+
+    # depois de modelado...
+    # vou precisar chamar control.py para enviar os dados modelados para o banco de dados json
+    control.create_lead(model_lead(name, company, email, stage))
+
+def list_leads():
+    leads = control.read_leads()
+
+    if not leads:
+        print("nenhum lead ainda")
+        return
+
+    print("\n# | Nome                 | Empresa           | E-mail")
+    for i, lead in enumerate(leads):
+        print(f"{i:02d}| {lead["name"]:<20} | {lead["company"]:<17} | {lead["email"]:<20} ")
+
+def search_leads():
+    query = input("Buscar por: ").strip().lower()
+    if not query:
+        print("Consulta vazia")
+        return
+
+    # nesse momento, irei enviar minha busca para control
+
+def export_leads():
+    print("export csv")
+
+def main():
+    while True:
+        print("\nMini CRM - 1ª aula - (adicionar/listar)")
+        print("[1] Adicionar lead")
+        print("[2] Listar leads")
+        print("[3] Buscar (nome/e-mail/empresa)")
+        print("[4] Exportar para CSV")
+        print("[0] Sair do programa")
+
+        opt = input("Escolha uma ação: ").strip()
+        if opt == "1":
+            add_lead()
+        elif opt == "2":
+            list_leads()
+        elif opt == "3":
+            search_leads()
+        elif opt == "4":
+            export_leads()
+        elif opt == "0":
+            print("Até mais")
+            break
+        else:
+            print("Opção inválida")
+
+if __name__ == "__main__":
+    main()
---
+++ aula09-mini-crm/control.py
@@ -0,0 +1,46 @@
+# repo.py
+
+from pathlib import Path
+import json
+
+DATA_DIR = Path(__file__).resolve().parent / "data"
+DATA_DIR.mkdir(exist_ok=True)
+DB_PATH = DATA_DIR / "leads.json"
+
+# CRUD
+# CREATE - create_lead()
+# READ - read_leads()
+# UPDATE
+# DELETE
+
+def read_leads():
+    if not DB_PATH.exists():
+        return []
+
+    try:
+        return json.loads(DB_PATH.read_text(encoding="utf-8"))
+    except json.JSONDecodeError:
+        # se corromper, começar vazio
+        return []
+
+def create_lead(lead_dict):
+    leads = read_leads() # lista de leads
+    leads.append(lead_dict)
+
+    DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent=2), encoding="utf-8")
+
+def read_leads_search(query):
+    leads = read_leads() # lista de leads
+    results = []
+
+    for i, lead in enumerate(leads):
+        txt_lead = f"{lead["name"]} {lead["company"]} {lead["email"]}".lower()
+
+        if query in txt_lead:
+            results.append(lead)
+
+    if not results:
+        print("Nada encontrado")
+        return
+    else:
+        return results
